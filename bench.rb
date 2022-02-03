99adac4751bc0dbe46158912fdab75f8ed45aaa9
Andrew Kane <andrew@ankane.org>
2022-02-03 15:37:38 -0800

    Test

---
+++ bench.rb
@@ -0,0 +1,6 @@
+require_relative "hello"
+
+hello = Hello.new
+10_000_000.times do
+  hello.world = 123
+end
---
+++ hello.cpp
@@ -0,0 +1,10 @@
+#include <rice/rice.hpp>
+
+using namespace Rice;
+
+extern "C"
+void Init_hello()
+{
+  Class rb_cHello = define_class("Hello");
+  rb_cHello.define_function("world=", [](Object value) {});
+}
